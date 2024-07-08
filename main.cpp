653e4b65ef136bd66e6e7b4045412db116fb27ee
Simon Asmar <asmarsimon@hotmail.com>
2024-07-08 18:41:08 -0400

    Hello Triangle ex1

--- main.cpp
+++ main.cpp
@@ -81,15 +81,18 @@ int main() {
 		"{\n"
 		"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 		"}\0";
-	float vertices[] = {
-		 0.5f,  0.5f, 0.0f,  // top right
-		 0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
+	float vertices[] = {//two triangles with common point on the origin
+		-0.5f,  0.5f, 0.0f,
+		-1.0f,  0.0f, 0.0f,
+
+		 0.5f,  0.5f, 0.0f,
+		 1.0f,  0.0f, 0.0f,
+
+		 0.0f,  0.0f, 0.0f,
 	};
-	unsigned int indices[] = {  // note that we start from 0!
-		0, 1, 3,   // first triangle
-		1, 2, 3    // second triangle
+	unsigned int indices[] = {
+		0, 1, 4,   // first triangle
+		2, 3, 4    // second triangle
 	};
 
 	//generate VAO(VBO,EBO) which VAO Also stores the vertex attributes
