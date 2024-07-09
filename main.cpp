bb3cdfac04a4ef13fcd433307850de2d5380776e
Simon Asmar <asmarsimon@hotmail.com>
2024-07-08 20:09:48 -0400

    colorful triangles + starting abstracting with classes

--- main.cpp
+++ main.cpp
@@ -70,25 +70,29 @@ int main() {
 	//other
 	const char* vertexShaderSource = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
+		"layout (location = 1) in vec3 aColor;\n"
+		"out vec3 ourColor;"
 		"void main()\n"
 		"{\n"
 		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"   ourColor = aColor;\n"
 		"}\0";
 
 	const char* fragmentShaderSource = "#version 330 core\n"
 		"out vec4 FragColor;\n"
+		"in vec3 ourColor;"
 		"void main()\n"
 		"{\n"
-		"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"FragColor = vec4(ourColor,1.0f);\n"
 		"}\0";
 	float vertices[] = {//two triangles with common point on the origin
-		-0.5f,  0.5f, 0.0f,
-		-1.0f,  0.0f, 0.0f,
+		-0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		-1.0f,  0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
 
-		 0.5f,  0.5f, 0.0f,
-		 1.0f,  0.0f, 0.0f,
+		 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		 1.0f,  0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
 
-		 0.0f,  0.0f, 0.0f,
+		 0.0f,  0.0f, 0.0f, 0.0f, 0.0f, 1.0f
 	};
 	unsigned int indices[] = {
 		0, 1, 4,   // first triangle
@@ -116,13 +120,23 @@ int main() {
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	//vertex attributes
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
 
 
 
-	//shaders
+	//shaders and uniforms
 	unsigned int shaderProgramId = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+
+	//int location = glGetUniformLocation(shaderProgramId, "ourColor");
+	//if (location == -1)
+	//	EXIT_MAIN("UNIFORM NAME DOESNT EXIST");
+	//glUseProgram(shaderProgramId);//need to use/bind the program to set its uniform value
+	//glUniform4f(location, 0.0f, 1.0f, 0.0f, 1.0f);
+
 
 	//main render loop
 	while (!glfwWindowShouldClose(window))
