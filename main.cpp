c1939356ffc6805c63de5b4269a7ae20c3f33201
Simon Asmar <asmarsimon@hotmail.com>
2024-07-15 17:39:52 -0400

    cleaned up main

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@
 
 using namespace std;
 
-#define EXIT_MAIN(message) {cout<<message<<endl; glfwTerminate(); return -1;}
 static constexpr int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
 
 //time
@@ -35,32 +34,7 @@ static glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
 
-	if (action == GLFW_PRESS)
-	{
-		switch(key) {
-			case GLFW_KEY_ESCAPE:
-				break;
-			case GLFW_KEY_LEFT_SHIFT:
-				shiftFunctionsEnabled = true;
-				break;
-			case GLFW_KEY_LEFT_CONTROL:
-				controlFunctionsEnabled = true;
-				break;
-		}
-	}
-	else if (action == GLFW_RELEASE) {
-		switch (key) {
-			case GLFW_KEY_LEFT_SHIFT:
-				shiftFunctionsEnabled = false;
-				break;
-			case GLFW_KEY_LEFT_CONTROL:
-				controlFunctionsEnabled = false;
-				break;
-		}
-	}
-}
 static void mouse_callback(GLFWwindow* window, double xpos,double ypos){
 	if (!controlFunctionsEnabled) {
 		
@@ -78,28 +52,7 @@ static void mouse_callback(GLFWwindow* window, double xpos,double ypos){
 	oldYPos = ypos;
 
 }
-
-
-int main() {
-
-	//setup
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGlGameEngine", NULL, NULL);
-	if (window == NULL)
-		EXIT_MAIN("Failed to create glfw window");
-
-	glfwMakeContextCurrent(window);
-	if (!gladLoadGL())
-		EXIT_MAIN("Failed to initialize GLAD");
-
-	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);//display rendering on the whole window
-
-	glfwGetCursorPos(window, &oldXPos, &oldYPos);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glfwSetCursorPosCallback(window, mouse_callback);
+static void initEditorInputs(GLFWwindow* window) {
 	Inputs::init(window);
 	Inputs::addKeyCallback({ GLFW_KEY_ESCAPE ,GLFW_PRESS }, [window]() {glfwSetWindowShouldClose(window, true); });
 	Inputs::addKeyCallback({ GLFW_KEY_LEFT_SHIFT ,GLFW_PRESS }, []() {shiftFunctionsEnabled = true; });
@@ -108,81 +61,112 @@ int main() {
 	Inputs::addKeyCallback({ GLFW_KEY_LEFT_CONTROL ,GLFW_RELEASE }, []() {controlFunctionsEnabled = false;  });
 
 	Inputs::addContinuousKeyCallback({ GLFW_KEY_W ,GLFW_PRESS }, []() {cameraPos += deltaTime * TRANSLATION_SENSITIVITY * cameraFront; });
-	Inputs::addContinuousKeyCallback({ GLFW_KEY_A ,GLFW_PRESS }, []() {cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * TRANSLATION_SENSITIVITY * deltaTime;});
+	Inputs::addContinuousKeyCallback({ GLFW_KEY_A ,GLFW_PRESS }, []() {cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * TRANSLATION_SENSITIVITY * deltaTime; });
 	Inputs::addContinuousKeyCallback({ GLFW_KEY_S ,GLFW_PRESS }, []() {cameraPos -= deltaTime * TRANSLATION_SENSITIVITY * cameraFront; });
 	Inputs::addContinuousKeyCallback({ GLFW_KEY_D ,GLFW_PRESS }, []() {cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * TRANSLATION_SENSITIVITY * deltaTime; });
+}
+static GLFWwindow* initOpenGlLibraries() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGlGameEngine", NULL, NULL);
+	if (window == NULL) {
+		cout << "Failed to create glfw window" << endl;
+		throw runtime_error{"GLFW init error"};
 
+	}
 
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGL()) {
+		cout << "Failed to load GLAD" << endl;
+		throw runtime_error{ "GLAD init error" };
+	}
+
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);//display rendering on the whole window
+
+	glfwGetCursorPos(window, &oldXPos, &oldYPos);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
 
 	glEnable(GL_DEPTH_TEST);
+	return window;
+}
+
+int main() {
+
+	//setup
+	GLFWwindow* window = initOpenGlLibraries();
+	initEditorInputs(window);
+
+	//variables
+	glm::vec3 lightPosition = { -2.0f, 0.0f, 1.65f };
+	glm::vec3 cubePosition = { 0.0f, 0.0f, 0.0f };
+	glm::vec3 secondCubePosition = { 0.0f, 2.0f, 0.0f };
+
+	float currentTime, previousTime;
+
 
 	//shaders and uniforms
 	Shader shader("shader.vs", "shader.fs");
 	Shader lightShader("unlitShader.vs", "unlitShader.fs");
-
-	glm::vec3 lightPosition = { -2.0f, 0.0f, 1.65f };
-	glm::vec3 cubePosition = { 0.0f, 0.0f, 0.0f };
-
-	Primitive cube = Primitive::cube(&shader,{cubePosition});
-	Primitive light = Primitive::cube(&shader, {lightPosition}, 0.1f);
-
-
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(60.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 	shader.useThenSetVec3f("uLightColor", 1.0f, 1.0f, 1.0f);
 	shader.useThenSetVec3f("uLightPosition", &lightPosition);
-
-	Texture texture1("container.jpg", &shader);
-	//Texture texture2("container.jpg", &shader, 2); //set the other image and link it to texture1 to be able to bind them at the same time
-	cube.linkChild(&texture1);
-	//cube.linkChild(&texture2);
-	//texture1.unbind();
-	//texture2.unbind();
+	shader.useThenSetMat4f("uProjection", &projection);
+	lightShader.useThenSetMat4f("uProjection", &projection);
 
 
-	//main render loop
-	float currentTime, previousTime;
+
+
+	//GameObjects setup
+	Primitive cube = Primitive::cube(&shader,{cubePosition});
+	Primitive secondCube = Primitive::cube(&shader, { secondCubePosition });
+
+	Primitive light = Primitive::cube(&lightShader, {lightPosition}, 0.1f);
+
+
+	//textures
+	Texture textureA("container.jpg", &shader);
+	cube.linkChild(&textureA);
+	Texture textureB("coolGuy.png", &shader);
+	secondCube.linkChild(&textureB);
+
+
 	currentTime = previousTime = glfwGetTime();
 
 	while (!glfwWindowShouldClose(window))
 	{
-		Inputs::checkContinuousInputs();
+		//clear
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		//update data
+		Inputs::checkContinuousInputs();
 		currentTime = glfwGetTime();
 		deltaTime = currentTime - previousTime;
 		previousTime = currentTime;
 
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		//CUBE
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, cubePosition);
+		//shader section
 		glm::mat4 view;
 		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(60.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
-		shader.useThenSetMat4f("uView", &view);
-		shader.useThenSetMat4f("uProjection", &projection);
-		shader.useThenSetMat4f("uModel", &model);
-		
-		cube.draw();
-
-		//LIGHT
-		auto lightModel = glm::translate(glm::mat4(1.0f), lightPosition);
-		lightShader.useThenSetMat4f("uView", &view);
-		lightShader.useThenSetMat4f("uProjection", &projection);
-		lightShader.useThenSetMat4f("uModel", &lightModel);
-
-		light.bind();
-		glDrawElements(GL_TRIANGLES, light.getNumberOfIndices(), GL_UNSIGNED_INT, 0);
-		light.unbind();
 
 		shader.useThenSetVec3f("uViewPosition", &cameraPos);
+		shader.useThenSetMat4f("uView", &view);
+		
+		cube.draw();
+		secondCube.draw();
+
+		//light shader section
+		lightShader.useThenSetMat4f("uView", &view);
+		light.draw();
 
 
+
+		//Swap buffer and check events
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 	}
 
-	EXIT_MAIN("");
 	return 0;
 }
