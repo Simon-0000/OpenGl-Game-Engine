43118a9be2c2aa573ff9e6124943c1cf80eae239
Simon Asmar <asmarsimon@hotmail.com>
2024-07-29 15:44:19 -0400

    files for the class

---
+++ Camera.cpp
@@ -0,0 +1,41 @@
+#include "Camera.hpp"
+
+Camera::Camera(const Transform& transform, float fovDeg, float aspectRatio, float renderDistance, float minRenderDistance):
+	Transform(transform), shaders_({}), fovRad(glm::radians(fovDeg)), aspectRatio(aspectRatio), renderDistance(renderDistance), minRenderDistance(minRenderDistance)
+{
+}
+
+void Camera::linkShader(Shader* shader)
+{
+	shaders_.push_back(shader);
+}
+
+void Camera::linkShaders(std::vector<Shader*> shaders)
+{
+	for (auto& shader : shaders)
+		linkShader(shader);
+}
+
+void Camera::localBind()
+{
+	glm::mat4 projection;
+	projection = glm::perspective(fovRad, aspectRatio, minRenderDistance, renderDistance);
+	for (auto& shader : shaders_)
+		shader->useThenSetMat4f("uProjection", &projection);
+}
+
+void Camera::localUnbind()
+{
+}
+
+bool Camera::tryUpdateModelMatrix() {
+	if (Transform::tryUpdateModelMatrix()) {
+		glm::mat4 view;
+		view = glm::lookAt(getPosition(), getPosition() + getForward(), getUp());
+		for (auto& shader : shaders_)
+			shader->useThenSetMat4f("uView", &view);
+
+		return true;
+	}
+	return false;
+}
---
+++ Camera.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "Transform.hpp"
+#include "Shader.hpp"
+#include "Bindable.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Camera : public Transform, public Bindable {
+public:
+	Camera(const Transform& transform, float fovDeg, float aspectRatio, float renderDistance, float minRenderDistance = 0.1f);
+	void linkShader(Shader* shader);
+	void linkShaders(std::vector<Shader*> shaders);
+	void localBind() override;
+	void localUnbind() override;
+	float fovRad, aspectRatio, renderDistance, minRenderDistance;
+protected:
+	bool tryUpdateModelMatrix() override;
+private:
+	std::vector<Shader*> shaders_;
+};
